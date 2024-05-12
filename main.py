9c5a195b760d4f970a15d8e11b0c1419bb029d8b
FinlayPunch <fpunch@student.saintaug.nsw.edu.au>
2024-05-13 09:36:36 +1000

    Added something that checks if you want to exit.

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 import gooeypie as gp
 
+
 def say_hello(event):
     hello_lbl.text = 'Hello Gooey Pie!'
 
+def check_exit():
+    ok_to_exit = app.confirm_yesno(None, 'Are you sure you want to close?', 'question')
+    return ok_to_exit
+
 app = gp.GooeyPieApp('Hello')
 app.width = 250
+app.title = 'Password Checker'
+
 
 hello_btn = gp.Button(app, 'Say Hello', say_hello)
 hello_lbl = gp.Label(app, '')
@@ -14,5 +21,6 @@ app.set_grid(2, 1)
 app.add(hello_btn, 1, 1, align='center')
 app.add(hello_lbl, 2, 1, align='center')
 
+app.on_close(check_exit)
 
 app.run()
