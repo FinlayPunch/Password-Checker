037677e11f5d837ca94bffc39df9d2bc7797b607
FinlayPunch <fpunch@student.saintaug.nsw.edu.au>
2024-06-19 14:57:23 +1000

    Code now checks to see if the password has a space in it. Now has a score system. Getting a good result in a part of the password adds 1 to the score. A set of stars are printed that are blacked or whited out depending on the total score.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import gooeypie as gp
+special_characters = "!@#$%^&*()-+?_=,<>/'"
 
 def load_common_words(file_path):
     with open(file_path, 'r') as file:
@@ -14,30 +15,63 @@ def toggle_mask(event):
 common_words = load_common_words('common_words.txt')
 
 def on_text_change(event):
-    
     text = secret.text
     print(text)
+    s = text
+    score = 0
 
     if is_common_word(text, common_words):
         label.text = "Common password/word"
     else:
         label.text = "Not a common password/word"
+        score += 1
 
     if text.isdigit():
         label_num.text = "Don't make it just numbers"
-    else:
-        label_num.text = "A combination of letters and numbers is good"
-        
-    if text.isalpha():
+    elif text.isalpha():
         label_num.text = "Try to have a combination of letters and numbers"
+    elif any(c.isdigit() for c in text) and any(c.isalpha() for c in text):
+        label_num.text = "It has a combination of letters and numbers, good"
+        score += 1
+    else:
+        label_num.text = "Try to have a combination of letters and numbers"
+
     if len(text) == 0:
-        label_length.text = "Nothing was entered"    
+        label_length.text = "Nothing"    
     if len(text) >= 1:
-        label_length.text = "Too short, try to make password 15+ characters in length"
+        label_length.text = "Too short"
     if len(text) >= 10:
-        label_length.text = "Decent length, try to make password 15+ characters in length"
+        label_length.text = "Decent length"
     if len(text) >= 15:
-        label_length.text = "Your password is 15+ characters long, a good length"
+        label_length.text = "Good length"
+        score += 1
+
+    if any(c in special_characters for c in s):
+        label_special.text = "Your password has special characters in it"
+        score += 1
+    else:
+        label_special.text = "Try to include some special characters in it (e.g. !, #, $)"
+
+    if ' ' in text:
+        label_space.text = "Having a space in it is good"
+        score += 1
+    else:
+        label_space.text = "Try putting a space in it"
+
+    if score == 0:
+        label_score.text = "☆☆☆☆☆"
+    elif score == 1:
+        label_score.text = "★☆☆☆☆"
+    elif score == 2:
+        label_score.text = "★★☆☆☆"
+    elif score == 3:
+        label_score.text = "★★★☆☆"
+    elif score == 4:
+        label_score.text = "★★★★☆"
+    elif score == 5:
+        label_score.text = "★★★★★"
+
+    print(score)
 
 
 def open_about_window(event):
@@ -62,13 +96,16 @@ check.add_event_listener('change', toggle_mask)
 label = gp.Label(app, '...')
 label_length = gp.Label(app, '...')
 label_num = gp.Label(app, '...')
+label_special = gp.Label(app, '...')
+label_space = gp.Label(app, '...')
+label_score = gp.Label(app, '...')
 
 about_btn = gp.Button(app, '?', open_about_window)
 about_btn.width = 5
 
 enter_btn = gp.Button(app, 'Enter', on_text_change)
 
-app.set_grid(6, 2)
+app.set_grid(9, 2)
 app.add(question, 1, 1)
 app.add(secret, 2, 1)
 app.add(enter_btn, 2, 2, align='center')
@@ -76,6 +113,9 @@ app.add(check, 3, 1)
 app.add(label, 4, 1, align='center')
 app.add(label_length, 5, 1, align='center')
 app.add(label_num, 6, 1, align='center')
+app.add(label_special, 7, 1, align='center')
+app.add(label_space, 8, 1, align='center')
+app.add(label_score, 9, 1, align='center')
 app.add(about_btn, 6, 2, align='center')
 
 about_window = gp.Window(app, 'About')
